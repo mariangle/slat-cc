61acc56a52759e43325caacb845be7c931c5123e
Maria <mariale171001@gmail.com>
2024-10-03 17:01:33 +0200

    feat: url metadata scraper

---
+++ url-metadata-scraper/index.ts
@@ -0,0 +1,69 @@
+import urlMetadata from "url-metadata";
+
+export function isValidURL(url: string) {
+  const urlRegex = /^(?:https?):\/\/[\w-]+(?:\.[\w-]+)+[\w.,@?^=%&:/~+#-]*$/;
+  return urlRegex.test(url);
+}
+
+interface FavIcon {
+  rel: string;
+  type?: string;
+  href: string;
+  sizes: string;
+}
+
+interface MetaData {
+  requestUrl: string;
+  url: string;
+  title: string;
+  image: string;
+  favicons: FavIcon[];
+  "og:title": string;
+  "og:image": string;
+  "og:image:type": string;
+  "og:image:width": string;
+  "og:image:height": string;
+  "twitter:image": string;
+  "twitter:image:alt": string;
+}
+
+function getLargestFavicon(favicons: FavIcon[]) {
+  if (favicons.length === 0) return;
+
+  const sortedFavicons = favicons.sort((a, b) => {
+    const [aWidth, aHeight] = a.sizes.split("x").map(Number);
+    const [bWidth, bHeight] = b.sizes.split("x").map(Number);
+
+    return bWidth * bHeight - aWidth * aHeight;
+  });
+
+  return sortedFavicons[0].href;
+}
+
+export async function scrapeMetadata(url: string) {
+  if (!isValidURL(url)) {
+    return {};
+  }
+
+  try {
+    const res = await urlMetadata(url);
+
+    const metadata = res as MetaData;
+
+    const title =
+      metadata["og:title"] || metadata["twitter:title"] || metadata.title;
+    const image =
+      metadata["og:image"] ||
+      metadata["twitter:image"] ||
+      getLargestFavicon(metadata.favicons) ||
+      metadata.image;
+
+    return {
+      title: title,
+      image: image,
+    };
+  } catch (e) {
+    console.error(e);
+    return {};
+  }
+}
